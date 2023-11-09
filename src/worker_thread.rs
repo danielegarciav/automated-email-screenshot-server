b8ffd96a32bef1cc4406498be0229130904a4f65
Daniel Garcia <danielegarciav@gmail.com>
2023-11-09 17:20:57 -0500

    refactor viewport scroller logic into new struct

--- src/worker_thread.rs
+++ src/worker_thread.rs
@@ -1,8 +1,10 @@
+mod viewport_scroller;
+
 use anyhow::Context;
 use itertools::Itertools;
 use std::path::PathBuf;
 use std::process::Command;
-use std::sync::{Arc, Condvar, Mutex};
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
@@ -11,52 +13,13 @@ use win_screenshot::capture::capture_window_ex;
 
 use uiautomation::actions::{Scroll, Transform, Window};
 use uiautomation::controls::{ControlType, DocumentControl, WindowControl};
-use uiautomation::variants::Variant;
-use uiautomation::{UIAutomation, UIElement};
+use uiautomation::UIAutomation;
+use viewport_scroller::ViewportScroller;
 
 use windows::Win32::Foundation::HWND;
-use windows::Win32::System::Variant::VARIANT;
-use windows::Win32::UI::Accessibility::{
-  IUIAutomation, IUIAutomationElement, IUIAutomationPropertyChangedEventHandler,
-  IUIAutomationPropertyChangedEventHandler_Impl, TreeScope_Element, UIA_ScrollPatternNoScroll as NoScroll,
-  UIA_ScrollVerticalScrollPercentPropertyId, UIA_PROPERTY_ID,
-};
 use windows::Win32::UI::HiDpi;
 
 use crate::eml_task::EmlTaskManager;
-
-#[windows_implement::implement(IUIAutomationPropertyChangedEventHandler)]
-struct ScrollEventHandler {
-  scroll_sync_channel: Arc<SyncChannel>,
-}
-
-impl IUIAutomationPropertyChangedEventHandler_Impl for ScrollEventHandler {
-  #[allow(non_snake_case)]
-  fn HandlePropertyChangedEvent(
-    &self,
-    _sender: Option<&IUIAutomationElement>,
-    _propertyid: UIA_PROPERTY_ID,
-    newvalue: &VARIANT,
-  ) -> ::windows::core::Result<()> {
-    // wrap around native windows variant
-    let new_value_variant: Variant = newvalue.clone().into();
-    let new_scroll_value: f64 = new_value_variant.try_into().unwrap();
-
-    let el: UIElement = _sender.unwrap().clone().into();
-    let pid = el.get_process_id().unwrap();
-
-    self.scroll_sync_channel.notify();
-
-    tracing::debug!(
-      "scroll event: {}, prop id: {:?}, sender: {:?}, pid: {}",
-      new_scroll_value,
-      _propertyid,
-      el,
-      pid,
-    );
-    windows::core::Result::Ok(())
-  }
-}
 
 fn init_dpi_awareness() -> anyhow::Result<()> {
   unsafe {
@@ -64,41 +27,6 @@ fn init_dpi_awareness() -> anyhow::Result<()> {
     thread::sleep(Duration::from_millis(30));
   };
   Ok(())
-}
-
-struct SyncChannel {
-  is_waiting: Mutex<bool>,
-  cond_var: Condvar,
-}
-
-impl SyncChannel {
-  fn new() -> Self {
-    Self {
-      is_waiting: Mutex::new(false),
-      cond_var: Condvar::new(),
-    }
-  }
-  fn wait(&self) {
-    *self.is_waiting.lock().unwrap() = true;
-    let (_guard, result) = self
-      .cond_var
-      .wait_timeout_while(
-        self.is_waiting.lock().unwrap(),
-        Duration::from_millis(10_000),
-        |is_waiting| *is_waiting,
-      )
-      .unwrap();
-
-    if result.timed_out() {
-      tracing::warn!("waiting timed out! (10 seconds)");
-    }
-  }
-
-  fn notify(&self) {
-    let mut is_waiting = self.is_waiting.lock().unwrap();
-    *is_waiting = false;
-    self.cond_var.notify_all();
-  }
 }
 
 pub fn start_worker_thread(task_manager: Arc<EmlTaskManager>) -> anyhow::Result<()> {
@@ -117,11 +45,6 @@ pub fn start_worker_thread(task_manager: Arc<EmlTaskManager>) -> anyhow::Result<
   std::fs::create_dir_all(eml_directory)?;
 
   let automation = UIAutomation::new()?;
-  let scroll_sync_channel = Arc::new(SyncChannel::new());
-  let com_event_handler_alloc = ScrollEventHandler {
-    scroll_sync_channel: scroll_sync_channel.clone(),
-  };
-  let com_event_handler: IUIAutomationPropertyChangedEventHandler = com_event_handler_alloc.into();
 
   loop {
     tracing::info!("receiving next task...");
@@ -134,12 +57,7 @@ pub fn start_worker_thread(task_manager: Arc<EmlTaskManager>) -> anyhow::Result<
         let _span_guard = task.span.map(|span| span.entered());
         let response = task.response;
         std::fs::write(&eml_file_path, task.eml_content).unwrap();
-        let result = perform_email_screenshot(
-          &automation,
-          &eml_file_path,
-          &com_event_handler,
-          &scroll_sync_channel,
-        );
+        let result = perform_email_screenshot(&automation, &eml_file_path);
         match result {
           Ok(..) => task_manager.report_task_completion(&task.id, "test path".to_string()),
           Err(..) => task_manager.report_task_failure(&task.id),
@@ -184,8 +102,6 @@ fn close_existing_windows(automation: &UIAutomation) -> anyhow::Result<()> {
 fn perform_email_screenshot(
   automation: &UIAutomation,
   eml_file_path: &PathBuf,
-  scroll_event_handler: &IUIAutomationPropertyChangedEventHandler,
-  scroll_sync_channel: &SyncChannel,
 ) -> anyhow::Result<DynamicImage> {
   close_existing_windows(automation)?;
 
@@ -233,19 +149,6 @@ fn perform_email_screenshot(
     .control_type(ControlType::Document)
     .find_first()?;
 
-  let a: IUIAutomation = automation.clone().into();
-  let el: IUIAutomationElement = viewport_element.clone().into();
-  unsafe {
-    a.AddPropertyChangedEventHandlerNativeArray(
-      &el,
-      TreeScope_Element,
-      None,
-      scroll_event_handler,
-      &[UIA_ScrollVerticalScrollPercentPropertyId],
-    )
-    .unwrap();
-  }
-
   let viewport_rect = viewport_element.get_bounding_rectangle()?;
 
   let viewport_top = viewport_rect.get_top() - window_rect.get_top();
@@ -256,16 +159,16 @@ fn perform_email_screenshot(
   let viewport_right = viewport_rect.get_right() - window_rect.get_left();
   let viewport_width = viewport_right - viewport_left;
 
-  let viewport_control: DocumentControl = viewport_element.try_into()?;
+  let viewport_control: DocumentControl = viewport_element.clone().try_into()?;
   let viewport_height_percentage = viewport_control.get_vertical_view_size()? / 100.0;
   let document_height = f64::round(viewport_height as f64 / viewport_height_percentage) as i32;
   let max_scroll_height = document_height - viewport_height;
   tracing::debug!(viewport_height, document_height, max_scroll_height);
 
+  let viewport_scroller = ViewportScroller::new(automation, &viewport_element);
   if viewport_control.get_vertical_scroll_percent()? > 0.0 && viewport_height_percentage < 1.0 {
     tracing::info!("rewinding viewport to top...");
-    viewport_control.set_scroll_percent(NoScroll, 0.0)?;
-    scroll_sync_channel.wait();
+    viewport_scroller.scroll_to(0.0)?;
     thread::sleep(Duration::from_millis(500));
   }
 
@@ -310,8 +213,7 @@ fn perform_email_screenshot(
     tracing::debug!(next_scroll_height, next_scroll_percent);
 
     tracing::info!("scrolling...");
-    viewport_control.set_scroll_percent(NoScroll, next_scroll_percent * 100.0)?;
-    scroll_sync_channel.wait();
+    viewport_scroller.scroll_to(next_scroll_percent * 100.0)?;
     thread::sleep(Duration::from_millis(500));
 
     let scroll_difference = next_scroll_height - scroll_height;
@@ -364,8 +266,8 @@ fn perform_email_screenshot(
     .stitch()
     .map_err(|msg| anyhow::anyhow!(msg))?;
 
-  unsafe { a.RemoveAllEventHandlers()? }
   tracing::info!("closing window now");
+  drop(viewport_scroller);
   close_existing_windows(automation)?;
   Ok(stitched_image)
 }
---
+++ src/worker_thread/viewport_scroller.rs
@@ -0,0 +1,144 @@
+use std::cell::OnceCell;
+use std::marker::PhantomData;
+use std::sync::{Condvar, Mutex, OnceLock};
+use std::thread_local;
+use std::time::Duration;
+
+use uiautomation::actions::Scroll;
+use uiautomation::controls::DocumentControl;
+use uiautomation::variants::Variant;
+use uiautomation::{UIAutomation, UIElement};
+
+use windows::Win32::System::Variant::VARIANT;
+use windows::Win32::UI::Accessibility::{
+  IUIAutomation, IUIAutomationElement, IUIAutomationPropertyChangedEventHandler,
+  IUIAutomationPropertyChangedEventHandler_Impl, TreeScope_Element, UIA_ScrollPatternNoScroll as NoScroll,
+  UIA_ScrollVerticalScrollPercentPropertyId, UIA_PROPERTY_ID,
+};
+
+/// The scroll sync channel, of which there's a single instance shared between our thread and the scroll event handler
+/// thread, is used by the main (scroll sender) thread to wait until a scroll has finished, and the by the scroll event
+/// handler thread to signal that a scroll has finished.
+static SCROLL_SYNC_CHANNEL: OnceLock<SyncChannel> = OnceLock::new();
+
+thread_local! {
+  /// The scroll event handler, of which there's a single instance for the main thread, is a COM object passed on to
+  /// the Window's native API, which will execute the callback (from a different thread) once a scroll event occurs.
+  /// It will notify the main (scroll sender) thread when a scroll has finished.
+  static SCROLL_EVENT_HANDLER: OnceCell<IUIAutomationPropertyChangedEventHandler> = OnceCell::new();
+}
+
+#[windows_implement::implement(IUIAutomationPropertyChangedEventHandler)]
+struct ScrollEventHandler;
+
+impl IUIAutomationPropertyChangedEventHandler_Impl for ScrollEventHandler {
+  #[allow(non_snake_case)]
+  fn HandlePropertyChangedEvent(
+    &self,
+    _sender: Option<&IUIAutomationElement>,
+    propertyid: UIA_PROPERTY_ID,
+    newvalue: &VARIANT,
+  ) -> ::windows::core::Result<()> {
+    // wrap around native windows variant
+    let new_value_variant: Variant = newvalue.clone().into();
+    let new_scroll_value: f64 = new_value_variant.try_into().unwrap();
+
+    let element: UIElement = _sender.unwrap().clone().into();
+    let pid = element.get_process_id().unwrap();
+
+    SCROLL_SYNC_CHANNEL.get().unwrap().notify();
+
+    tracing::debug!(
+      "scroll event - new value: {}, prop id: {:?}, sender: {:?}, pid: {}",
+      new_scroll_value / 100.0,
+      propertyid.0,
+      element,
+      pid,
+    );
+    windows::core::Result::Ok(())
+  }
+}
+
+struct SyncChannel {
+  is_waiting: Mutex<bool>,
+  cond_var: Condvar,
+}
+
+impl SyncChannel {
+  fn new() -> Self {
+    Self {
+      is_waiting: Mutex::new(false),
+      cond_var: Condvar::new(),
+    }
+  }
+  fn wait(&self) {
+    *self.is_waiting.lock().unwrap() = true;
+    let (_guard, result) = self
+      .cond_var
+      .wait_timeout_while(
+        self.is_waiting.lock().unwrap(),
+        Duration::from_millis(10_000),
+        |is_waiting| *is_waiting,
+      )
+      .unwrap();
+
+    if result.timed_out() {
+      tracing::warn!("waiting timed out! (10 seconds)");
+    }
+  }
+
+  fn notify(&self) {
+    let mut is_waiting = self.is_waiting.lock().unwrap();
+    *is_waiting = false;
+    self.cond_var.notify_all();
+  }
+}
+
+pub struct ViewportScroller<'a> {
+  // Pretend like we store a shared reference to the automation and element objects. This tells the compiler that
+  // a `ViewportScroller` should not outlive the references passed on to `new()`. Otherwise, a `ViewportScroller`
+  // could accidentally prevent their backing memory from being released long after they're no longer used.
+  phantom: PhantomData<(&'a UIAutomation, &'a UIElement)>,
+  win_automation: IUIAutomation,
+  viewport_control: DocumentControl,
+}
+
+impl<'a> ViewportScroller<'a> {
+  pub fn new(automation: &'a UIAutomation, viewport_element: &'a UIElement) -> Self {
+    let win_automation: IUIAutomation = automation.clone().into();
+    let win_element: IUIAutomationElement = viewport_element.clone().into();
+    let viewport_control: DocumentControl = viewport_element.clone().try_into().unwrap();
+    SCROLL_SYNC_CHANNEL.get_or_init(SyncChannel::new);
+    SCROLL_EVENT_HANDLER.with(|scroll_event_handler| unsafe {
+      win_automation
+        .AddPropertyChangedEventHandlerNativeArray(
+          &win_element,
+          TreeScope_Element,
+          None,
+          scroll_event_handler.get_or_init(|| ScrollEventHandler.into()),
+          &[UIA_ScrollVerticalScrollPercentPropertyId],
+        )
+        .unwrap();
+    });
+    Self {
+      phantom: PhantomData,
+      win_automation,
+      viewport_control,
+    }
+  }
+
+  pub fn scroll_to(&self, position: f64) -> anyhow::Result<()> {
+    // TODO: lock the sync channel from getting notified until we start waiting
+    // Currently there's a highly unlikely but non-zero chance that the scroll finishes before we start
+    // waiting for a signal, which would lead to a deadlock.
+    self.viewport_control.set_scroll_percent(NoScroll, position)?;
+    SCROLL_SYNC_CHANNEL.get().unwrap().wait();
+    Ok(())
+  }
+}
+
+impl<'a> Drop for ViewportScroller<'a> {
+  fn drop(&mut self) {
+    unsafe { self.win_automation.RemoveAllEventHandlers().unwrap() }
+  }
+}
